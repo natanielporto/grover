19981b6c1b7fd6886d52916e5789f4a23f75a04b
Nataniel Porto <natanielkporto@gmail.com>
2021-12-18 11:33:48 -0300

    Fixes eslint rules for typescript

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavbarContainer } from './styles';
+
+export default function Navbar(): JSX.Element {
+  return (
+    <NavbarContainer>
+      "The New York Times" article search application
+    </NavbarContainer>
+  );
+}
---
+++ src/components/navbar/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  background-color: #000;
+  color: #fff;
+`;
